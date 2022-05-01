a458f13ecd98e0e42d02d9881fde263bb0a5b4d3
Ethan Gonzalez <104691236+legendofethan123@users.noreply.github.com>
2022-05-01 10:58:56 -0500

    Create FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,16 @@
+class FizzBuzz {
+
+  public static void main(String[] args) {
+
+  for (int i = 0; i <= 100; i++) {
+    if (i % 3 == 0) 
+     System.out.println(i +" "+ "is divisble by three!");
+     else 
+     System.out.println(i +" "+ "is not divisble by three!");
+
+    }
+
+
+
+    }
+  }
